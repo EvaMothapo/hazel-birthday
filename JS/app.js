a2c882c7eb8325432a076ac096fabeabadfda6bf
Eva Mothapo <matlalaeva6@gmail.com>
2026-06-07 21:45:54 +0200

    Added touchups

--- JS/app.js
+++ JS/app.js
@@ -34,19 +34,6 @@ window.addEventListener("load", () => {
     "Happy Birthday Linea De vida✨🎂 ",
   ];
 
-  let topBar = document.getElementById("topBar");
-  let bottomBar = document.getElementById("bottomBar");
-
-  setTimeout(() => {
-    topBar.style.opacity = "0";
-    bottomBar.style.opacity = "0";
-
-    setTimeout(() => {
-      topBar.style.display = "none";
-      bottomBar.style.display = "none";
-    }, 1000);
-  }, 4000);
-
   toggleBtn.addEventListener("click", () => {
     if (isPlaying) {
       music.pause();
@@ -88,7 +75,7 @@ window.addEventListener("load", () => {
   }
 
   let text =
-    "Happy Birthday Bestie!! ❤️ I hope your day is as gorgeous as you are, I love you loads!";
+    "Happy Birthday Bestie!!   I hope your day is as gorgeous as you are, I love you loads!";
   let typeIndex = 0;
 
   function typeEffect() {
@@ -106,8 +93,20 @@ window.addEventListener("load", () => {
         button.addEventListener("click", () => {
           let finalMessage = document.createElement("h2");
           finalMessage.id = "final-msg";
-          finalMessage.innerHTML = "You Mean The World To Me ♥️";
-
+          finalMessage.innerHTML =
+            "As someone who struggled to make friends, I didn't expect I'd manage to have a friend like you ❤️.<br><br>" +
+            "I remember the day we started being friends. I thought, 'Oh well! She's gonna break my heart 💔 like others did,' but nope, you proved me wrong.<br><br>" +
+            "You showed me that there's still good people ✨ out there who will bring you calmness 😶‍🌫️ in a storm 🌪️, laughter 🤣 on my bad 👎🏼 days, and real warmth into my life 🫠.<br><br>" +
+            "It's so crazy how we started as strangers and today we're more like sisters 👯‍♀️ than friends.<br><br>" +
+            "And what makes it even more beautiful and meaningful is everything we've been through together; the ups and downs, the stressful days, the moments that felt like we were being tested, and the ones that made us laugh when we shouldn't have.<br><br>" +
+            "These experiences built something real, more like a connection ❤️ that actually means a lot for the both of us.<br><br>" +
+            "I love how kind but still tough you are, how you know how to walk away from situations that don't serve you. I learnt to know when to remove myself because of you.<br><br>" +
+            "You always know how to make my bad days brighter, to make ordinary things feel so special without even doing much.<br><br>" +
+            "I'm really glad our paths crossed and I hope we grow stronger and wiser.<br><br>" +
+            "Re fetile dilo tse dintshi and we're still strong irregardless. Not even the distance could separate our bond ❤️.<br><br>" +
+            "You Mean The World To Me ♥️";
+            
+            
           finalMessage.style.opacity = "0";
           finalMessage.style.transform = "scale(0.8)";
           finalMessage.style.transition = "all 1s ease";
@@ -118,6 +117,25 @@ window.addEventListener("load", () => {
             finalMessage.style.opacity = "1";
             finalMessage.style.transform = "scale(1)";
           }, 100);
+
+          // Birthday age reveal
+          let ageReveal = document.createElement("h3");
+          ageReveal.innerHTML = "🎂 Level 24 Unlocked! ✨";
+          ageReveal.style.opacity = "0";
+          ageReveal.style.marginTop = "15px";
+          ageReveal.style.color = "white";
+          ageReveal.style.transition = "opacity 2s ease";
+
+          finalSection.appendChild(ageReveal);
+
+          setTimeout(() => {
+            ageReveal.style.opacity = "1";
+          }, 1200);
+
+          // Extra confetti burst
+          for (let i = 0; i < 30; i++) {
+            setTimeout(createConfetti, i * 100);
+          }
 
           button.remove();
         });
